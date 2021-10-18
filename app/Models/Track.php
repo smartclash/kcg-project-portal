04bb74db2535e8e24310f547efc81a09bc00e678
Karan Sanjeev <yo@alphaman.me>
2021-10-18 15:22:01 +0530

    Add tracks table

---
+++ app/Models/Track.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Track
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $content
+ * @property string|null $due_date
+ * @property string|null $lock_date
+ * @property int $locked
+ * @property int|null $team_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Submission[] $submissions
+ * @property-read int|null $submissions_count
+ * @property-read \App\Models\Team|null $team
+ * @method static \Illuminate\Database\Eloquent\Builder|Track newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Track newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Track query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Track whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Track whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Track whereDueDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Track whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Track whereLockDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Track whereLocked($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Track whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Track whereTeamId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Track whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Track extends Model
+{
+    use HasFactory;
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class);
+    }
+
+    public function submissions()
+    {
+        return $this->hasMany(Submission::class);
+    }
+}
---
+++ database/migrations/2021_10_18_061855_create_tracks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('content');
+
+            $table->timestamp('due_date')->nullable();
+            $table->timestamp('lock_date')->nullable();
+
+            $table->boolean('locked');
+
+            $table->bigInteger('team_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracks');
+    }
+}
