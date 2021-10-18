5ae412f504bff49249e9fbfdc8eefdf170fb629f
Karan Sanjeev <yo@alphaman.me>
2021-10-18 15:34:45 +0530

    Register middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Roles\AdminOnly;
+use App\Http\Middleware\Roles\HeadOnly;
+use App\Http\Middleware\Roles\MentorOnly;
+use App\Http\Middleware\Roles\StudentOnly;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -63,5 +67,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'only.admin' => AdminOnly::class,
+        'only.head' => HeadOnly::class,
+        'only.mentor' => MentorOnly::class,
+        'only.student' => StudentOnly::class,
     ];
 }
